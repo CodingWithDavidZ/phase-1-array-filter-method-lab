eb19dc748efb880c62cfabae199c490029392a35
David Zombar <davidzombar@gmail.com>
2021-07-05 13:22:50 -0600

    commit tree cd04f319ac539493250362f1bb667527feb72e0e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+//make a function names "findMatching"
+function findMatching(collection, name) {
+  return collection.filter(
+    (match) => match.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(collection, name) {
+  const lengthName = name.length;
+  return collection.filter((match) => match.slice(0, lengthName) === name);
+}
+
+function matchName(collection, str) {
+  return collection.filter((match) => match.name === str);
+}
